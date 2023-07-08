154b5d8539de1da72565735fa3cc6466fe13c949
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-07-08 23:01:10 +0800

    structuring forms

--- BAS_project/src/App.jsx
+++ BAS_project/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import PersonalInfo from './component/personalInfoForm'
+
 import './App.css'
 
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((prev) => prev + 1)}>
-          Count is {count}
-        </button>
-      </div>
+      {/* <Stepper /> */}
+      {/* <UserSignUp/> */}
+      {/* <AccountDetails/> */}
+      {/* <PersonalDetails/> */}
+      <PersonalInfo />
     </>
   )
 }
