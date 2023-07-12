3ab6c054e987bdc971514e570a67a7abbd7ae80a
Jdeo Barrera <jdeo.barrera@gmail.com>
2023-07-12 19:21:04 +0800

    change to branch

--- BAS_project/src/App.jsx
+++ BAS_project/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import UserNavbar from "./components/UserNavbar"
+import UserSidebar from "./components/UserSidebar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((prev) => prev + 1)}>
-          Count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div>
+    <UserNavbar/>
+    <UserSidebar/>
+    </div>
+    
+);
 }
 
 export default App
---
+++ BAS_project/src/components/userSidebar.jsx
@@ -0,0 +1,18 @@
+function Sidebar() {
+  return (
+    <div>
+      <ul>
+        <li>
+           <button>Dashboard</button></li>
+        <li>
+            <button className="">Appointment</button></li>
+        <li>
+           <button>History</button></li>
+        <li>
+           <button>Account Setting</button></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
