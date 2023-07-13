94de7060b959ac1ff4b5f75cafc254bde0051e2e
Jerricco John Cabunagan <66761274+Jeco99@users.noreply.github.com>
2023-07-13 23:27:22 +0800

    Revert "user navbar & sidebar components"

--- BAS_project/src/App.jsx
+++ BAS_project/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
+import "./App.css";
 import LogIn from "./components/LogIn";
 import BarangaySignUp from "./components/BarangaySignUp";
 import UserSignUp from "./component/user/UserSignUp/userSignUp"
-import UserNavbar from "./component/user/UserNavbarSidebar/userNavbar"
-import UserSidebar from "./component/user/UserNavbarSidebar/userSidebar"
-import Sidebartest from "./component/user/UserNavbarSidebar/sideBar"
-import Navbartest from "./component/user/UserNavbarSidebar/navBar"
 
 function App() {
   return (
     <>
-      
-      <UserNavbar />
-      <Sidebartest />
+      <LogIn />
+      {/* <BarangaySignUp /> */}
+      <UserSignUp />
     </>
   );
 }
